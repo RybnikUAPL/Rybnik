7d8a42383262bce937d92848484f038ac9624a50
Anatolii <155391612+RybnikUAPL@users.noreply.github.com>
2024-02-22 22:05:51 +0100

    Add files via upload

---
+++ js/my.scripts.js
@@ -0,0 +1,39 @@
+
+// jQuery код
+    $(document).ready(function(){
+        // Single page nav
+        $('.tm-main-nav').singlePageNav({
+            'currentClass' : "active",
+            offset : 20
+        });
+    });
+
+    // Magnific pop up
+    $('.tm-gallery-1').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {enabled:true}
+    });
+
+    $('.tm-gallery-2').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {enabled:true}
+    });
+
+    $('.tm-gallery-3').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {enabled:true}
+    });
+
+    $('.tm-gallery-4').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {enabled:true}
+    });
+
+    $('.tm-current-year').text(new Date().getFullYear());
+
+
+
