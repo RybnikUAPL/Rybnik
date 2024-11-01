dd135e863dc5bd21f5adcf57f630683a2e0fa6b9
AnatoliiSoloviov <anatolsoloviov@gmail.com>
2024-11-01 17:01:19 +0100

    123

--- js/happyNewYear.js
+++ js/happyNewYear.js
@@ -4,9 +4,9 @@ function updateCountdown() {
     const timeDifference = newYear - now;
 
     const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0');
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000).toString().padStart(2, '0');
 
     document.getElementById('countdown-timer').textContent =
         `${days} дн ${hours} ч ${minutes} мин ${seconds} сек`;
